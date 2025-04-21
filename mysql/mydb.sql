8bd3c99bc7c8e1040d4d1b329a576907c1f264b0
shiro_neko <hibikihe@gmail.com>
2025-04-21 23:30:50 +0800

    添加 mysql 初始数据

---
+++ mysql/mydb.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : shiro
+ Source Server Type    : MySQL
+ Source Server Version : 90200 (9.2.0)
+ Source Host           : localhost:20306
+ Source Schema         : mydb
+
+ Target Server Type    : MySQL
+ Target Server Version : 90200 (9.2.0)
+ File Encoding         : 65001
+
+ Date: 21/04/2025 23:29:39
+*/
+
